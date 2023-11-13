f48edeb327809ee7deaa878c4769491cc342c351
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2023-11-14 00:20:25 +0530

    Add files via upload

---
+++ print a pattern of asterisks in a descending order.py
@@ -0,0 +1,11 @@
+#print a pattern of asterisks in a descending order
+
+rows = 5
+
+for i in range(rows + 1, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+
+    print(" ")
+
+    
